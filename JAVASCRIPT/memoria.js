1d826a46bfab3b7175ad435558c58905cc72e6b3
fcuya13 <20203263@aloe.ulima.edu.pe>
2024-01-16 12:33:20 -0500

    Ejercicio de Juego Memoria
    Ejercicio con javascript, css y html

--- JAVASCRIPT/memoria.js
+++ JAVASCRIPT/memoria.js
@@ -1,5 +1,9 @@
 let simbolosDisponibles = ["&#128526", "&#128513","&#128565"]
+
 let listaCasillas = []
+
+let numeroTurno = 1
+let casillaTurnoAnterior = null
 
 function crearCasillas() {
 
@@ -27,10 +31,45 @@ function ponerSimboloCasillas() {
         for (let j = 0; j < 3; j++) {
             const but = document.getElementById( i + "_" + j)
             const casilla = devolverCasilla(i, j)
-            but.innerHTML = casilla.simbolo
+            if (casilla.mostrandoSimbolo) {
+                but.innerHTML = casilla.simbolo
+            } else {
+                but.innerHTML = "UL"
+            }
         }
     }
 }
+
+function casillaOnClick(row, col) {
+    // 1. Obtener datos de la casilla que hizo click
+    const casillaSeleccionada = devolverCasilla(row, col)
+    if (numeroTurno == 1) {
+        casillaSeleccionada.mostrandoSimbolo = true
+        casillaTurnoAnterior = casillaSeleccionada
+        numeroTurno = 2
+        ponerSimboloCasillas()
+    } else {
+        casillaSeleccionada.mostrandoSimbolo = true
+        // 2. Verificación si son iguales los símbolos
+        if (casillaTurnoAnterior.simbolo != casillaSeleccionada.simbolo) {
+            ponerSimboloCasillas()  
+
+            setTimeout(function() {
+                // Deben ocultarse los dos
+                casillaSeleccionada.mostrandoSimbolo = false
+                casillaTurnoAnterior.mostrandoSimbolo = false
+                ponerSimboloCasillas()
+                casillaTurnoAnterior = null
+                numeroTurno = 1
+            }, 2000)
+        } else {
+            // Caso Correcto
+            casillaTurnoAnterior = null
+            numeroTurno = 1 
+            ponerSimboloCasillas()
+        }
+    }
+}       
 
 function main() {
     crearCasillas()
